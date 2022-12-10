ec31b25babddbc06cab20c242b15b8e2ff830dbc
Kalpakbaev <kozha_1992@mail.ru>
2022-12-10 07:56:44 +0500

    palindrom

---
+++ palindrom/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+int palindrom (){
+    Console.WriteLine("Введите пятизначное число");
+    int entered_dgit = Convert.ToInt32(Console.ReadLine());
+    int first_digit = entered_dgit / 10000;
+    int last_digit = entered_dgit % 10;
+   if (first_digit == last_digit){
+    Console.Write("Да число палиндром > ");
+    return entered_dgit;
+   } else
+    Console.Write("Нет число не палиндром > ");
+    return entered_dgit;
+   }
+int res = palindrom();
+Console.WriteLine(res);
